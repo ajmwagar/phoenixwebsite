70c4f9bcb73f6126e803b31955c6633eb18b3077
Nathan <nlaha@outlook.com>
2018-08-26 00:51:36 -0700

    added landing page

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,29 @@
     
     <!-- Main content starts here!-->
     <div class="container">
-        <div class="row">
-            <div class="column">
-              <h1 class="landing-title">Phoenix</h1>
-            </div>
-        </div>
+      <div class="row">
+          <div class="column">
+            <h1 class="landing-title text-pop-up-top">Phoenix</h1>
+          </div>
+      </div>
     </div>
-
-
+    <div class="content">
+      <div class="container">
+        <div class="row">
+          <div class="five columns centertext">
+            <h1>This is Phoenix</h1>
+            <button class="sp-btn"> Invite Me!</button>
+          </div>
+          <div class="seven columns">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Posuere sollicitudin aliquam ultrices sagittis. In nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Adipiscing bibendum est ultricies integer quis auctor elit sed. Sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum. Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Magna sit amet purus gravida quis blandit. Id diam vel quam elementum. Sed sed risus pretium quam vulputate dignissim suspendisse in. Lorem donec massa sapien faucibus et molestie ac. Vel facilisis volutpat est velit egestas dui id ornare. Adipiscing diam donec adipiscing tristique risus nec feugiat. Cursus in hac habitasse platea dictumst. Quam adipiscing vitae proin sagittis nisl rhoncus. Sed risus pretium quam vulputate dignissim suspendisse in est. Ornare arcu dui vivamus arcu felis bibendum ut tristique.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-require("./assets/css/main.css");
 require("./assets/css/skeleton.css");
 require("./assets/css/normalize.css");
+require("./assets/css/main.css");
 
 new Vue({
   render: h => h(App)
